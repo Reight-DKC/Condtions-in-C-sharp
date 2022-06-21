b8571dfc14583c0b8b4fc9b75bfcdf799ffa5157
Reight-DKC <reightultimate@gmail.com>
2022-06-20 19:41:11 -0600

    Conditions practice complete

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("C# Conditions");
+Console.WriteLine("Enter student grade");
+double grade = Double.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Did student paid (y/n)");
+bool paid;
+string input = Console.ReadLine() ?? "n";
+if (input == "y")
+{
+    paid = true;
+  }  else {
+  paid = false;
+  }
+
+  if (grade >= 60)
+  {
+
+    if (paid)
+    {
+        Console.WriteLine("Congratulations!");
+        } else {
+            Console.WriteLine("You need to pay graduation fees.");
+            }
+            } else {
+                Console.WriteLine("Did not graduated!");
+                }
+
+                Console.WriteLine("Program ended!");
